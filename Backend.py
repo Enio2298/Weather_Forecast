cbc58d7906cd97af0e326edf9d8dfc5a73afeb82
Enio2298 <131815407+Enio2298@users.noreply.github.com>
2024-08-28 23:44:25 -0600

    Add files via upload

---
+++ Backend.py
@@ -0,0 +1,17 @@
+import requests
+
+API_key = "3810ea84461bc74a75b50d7c534fbe69"
+
+
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
